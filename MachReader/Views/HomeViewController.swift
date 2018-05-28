0366abcc420d45116c5edb8028b2c677f9a26f4f
ngo275 <ngo38114@gmail.com>
2018-05-28 22:49:24 +0900

    open pdf from iCloud drive

--- MachReader/Views/HomeViewController.swift
+++ MachReader/Views/HomeViewController.swift
@@ -18,20 +18,36 @@ class HomeViewController: UIViewController {
 
     // MARK: - Private methods
     
-    private func openReader() {
-        guard let path = Bundle.main.path(forResource: "sample", ofType: "pdf") else { return }
-        let pdfURL = URL(fileURLWithPath: path)
-        guard let document = PDFDocument(url: pdfURL) else { return }
-        guard let hashID = SHA1.hexString(fromFile: path) else { return }
-        
+    private func openReader(with url: URL) {
+        guard let document = PDFDocument(url: url) else { return }
+        guard let hashID = SHA1.hexString(fromFile: url.path) else { return }
+
         let vc = PdfReaderViewController.instantiate(document: document, hashID: hashID)
         navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    private func openSample() {
+        guard let path = Bundle.main.path(forResource: "sample", ofType: "pdf") else { return }
+        let pdfURL = URL(fileURLWithPath: path)
+        openReader(with: pdfURL)
     }
     
     // MARK: - IBActions
     
     @IBAction func handleOpenICloud(_ sender: Any) {
-        openReader()
+        let documentPickerController = UIDocumentPickerViewController(documentTypes: ["org.idpf.epub-container", "com.adobe.pdf"], in: .import)
+        documentPickerController.delegate = self
+        present(documentPickerController, animated: true)
+    }
+}
+
+// MARK: - UIDocumentPickerDelegate
+extension HomeViewController: UIDocumentPickerDelegate {
+    func documentPicker(_ controller: UIDocumentPickerViewController, didPickDocumentAt url: URL) {
+        openReader(with: url)
     }
     
+    func documentPickerWasCancelled(_ controller: UIDocumentPickerViewController) {
+        print("cancelled")
+    }
 }
